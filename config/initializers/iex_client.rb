23182db5ec6064dcade4a8765d26f0d2af7d9c0c
micoongkeko <ivongkeko@gmail.com>
2021-09-03 19:08:06 +0800

    Added iex initializer

---
+++ config/initializers/iex_client.rb
@@ -0,0 +1,5 @@
+IEX::Api.configure do |config|
+    config.publishable_token = 'pk_63c24619b1dc4a1da70647a6a5e15647' # defaults to ENV['IEX_API_PUBLISHABLE_TOKEN']
+    config.secret_token = 'sk_007deba5592945e7a15e5e63e656840c' # defaults to ENV['IEX_API_SECRET_TOKEN']
+    config.endpoint = 'https://cloud.iexapis.com/v1' # use 'https://sandbox.iexapis.com/v1' for Sandbox
+end  
