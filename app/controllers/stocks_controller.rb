c1b4bfdf8164a63a030d7b732aaa52bf32147844
micoongkeko <ivongkeko@gmail.com>
2021-09-09 20:33:25 +0800

    Buy and sell working. Need more views of user dashboard etc

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -7,79 +7,53 @@ class StocksController < ApplicationController
     end
 
     def transact
-        # @user = User.find(params[:id])
-        # @transaction = Transaction.find(params[:id])
-        # @stock = Stock.find(params[:stock_id])
-        # byebug
-
-        # BACK UP LOL
-        # @transaction = Transaction.create!(
-        #     stock_id: params[:transact][:stock_id],
-        #     user_id: current_user.id,
-        #     buy: params[:transact][:buy],
-        #     sell: params[:transact][:sell],
-        #     price: Stock.find(params[:transact][:stock_id]).price,
-        #     quantity: params[:transact][:quantity]
-        # )
-        # @stock_bought = Transaction.where(user_id:current_user.id,stock_id:@stock.id, buy:true).sum(:quantity)
-        # @stock_sold = Transaction.where(user_id:current_user.id,stock_id:@stock.id, sell:true).sum(:quantity)
-        # @stock_active = @stock_bought - @stock_sold
-        # @user = User.find(@transaction.user_id)
-        # @stock = Stock.find(@transaction.stock_id)
-        # if @transaction.buy == true
-        #     @user.balance -= (@transaction.price * @transaction.quantity)
-        #     @stock.quantity -= @transaction.quantity
-        #     @user.save!
-        #     @stock.save!
-        # elsif @transaction.sell == true && @stock_active >= @stock.quantity
-        #     @user.balance += (@transaction.price * @transaction.quantity)
-        #     @stock.quantity += @transaction.quantity
-        #     @user.save!
-        #     @stock.save!
-
 
         # TAKE TWO IF BLAH BLAH
-        
         @stock_bought = Transaction.where(user_id:current_user.id,stock_id:params[:transact][:stock_id], buy:true).sum(:quantity)
         @stock_sold = Transaction.where(user_id:current_user.id,stock_id:params[:transact][:stock_id], sell:true).sum(:quantity)
         @stock_active = @stock_bought - @stock_sold
         @user = User.find(current_user.id)
         @stock = Stock.find(params[:transact][:stock_id])
 
-        
-        if params[:transact][:buy] == true
+        if params[:transact][:buy] == "1"
             @transaction = Transaction.create!(
                 stock_id: @stock.id,
                 user_id: current_user.id,
                 buy: params[:transact][:buy],
                 sell: params[:transact][:sell],
                 price: @stock.price,
-                quantity: params[:transact][:quantity]
+                quantity: params[:transact][:quantity].to_i
             )
             @user.balance -= (@transaction.price * @transaction.quantity)
             @stock.quantity -= @transaction.quantity
             @user.save!
             @stock.save!
-        elsif (params[:transact][:sell] == true) && (@stock_active >= params[:transact][:quantity])
+
+            redirect_to users_path
+            flash[:success] = "Transaction successful"
+        elsif (params[:transact][:sell] == "1") && (@stock_active >= params[:transact][:quantity].to_i)
             @transaction = Transaction.create!(
                 stock_id: @stock.id,
                 user_id: current_user.id,
                 buy: params[:transact][:buy],
                 sell: params[:transact][:sell],
                 price: @stock.price,
-                quantity: params[:transact][:quantity]
+                quantity: params[:transact][:quantity].to_i
             )
             @user.balance += (@transaction.price * @transaction.quantity)
             @stock.quantity += @transaction.quantity
             @user.save!
-            @stock
-        elsif @stock_active < params[:transact][:quantity].to_i
-            flash[:notice] = "You do not have enough of this stock to sell."
+            @stock.save!
+
+            redirect_to users_path
+            flash[:success] = "Transaction successful"
+        elsif (params[:transact][:sell] == "1") && (@stock_active < params[:transact][:quantity].to_i)
+            redirect_to users_path
+            flash[:alert] = "You do not have enough of this stock to sell."
         end
 
         # byebug
-        redirect_to users_path
-        flash[:notice] = "Transaction successful"
+
     end
 
 
